974ffa53c6ef0bdd7b912715b68b740bcaead6fc
karlbin <bin.shanhong@ricequant.com>
2020-11-07 23:37:56 +0800

    delete

--- master.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: your name
- * @Date: 2020-11-07 23:33:45
- * @LastEditTime: 2020-11-07 23:33:56
- * @LastEditors: your name
- * @Description: In User Settings Edit
- * @FilePath: \git-learning\master.js
- */
-console.log('nihao')
