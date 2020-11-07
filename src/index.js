f304c074337fdc0ee0bb5e6310c2884f9ac0b1da
karlbin <bin.shanhong@ricequant.com>
2020-11-07 23:15:55 +0800

    add function checkStr

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+let p = "abcd", q = "ad"
+function checkStr(source, target) {
+  let temp = {}
+  for (let i = 0; i < source.length; i++) {
+    const item = source[i];
+    temp[item] = 1
+  }
+  for (let j = 0; j < target.length; j++) {
+    if (!temp[target[j]]) { return false }
+  }
+  return true
+}
+checkStr(p, q)
