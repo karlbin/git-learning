b56b740ae3561d8b5a1c0174b0564b7c8dae2619
karlbin <bin.shanhong@ricequant.com>
2020-11-07 23:34:51 +0800

    add master

---
+++ master.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-07 23:33:45
+ * @LastEditTime: 2020-11-07 23:33:56
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \git-learning\master.js
+ */
+console.log('nihao')
